18e79d25a40dc501cddcac496a6ab8bb0ec0adce
ReenBo <pavmydj@gmail.com>
2022-06-13 16:24:58 +0300

    fixed the class NewInput

--- Assets/Scripts/Player/Managers/NewInputSystem.cs
+++ Assets/Scripts/Player/Managers/NewInputSystem.cs
@@ -24,12 +24,13 @@ public class NewInputSystem : MonoBehaviour
 
     private void Update()
     {
-        Vector2 moveDirection = _input.Player.Move.ReadValue<Vector2>();
-        Move(moveDirection);
+        Move();
     }
 
-    private void Move(Vector2 direction)
+    private void Move()
     {
+        Vector2 direction = _input.Player.Move.ReadValue<Vector2>();
+
         float scaledMoveSpeed = _moveSpeed * Time.deltaTime;
 
         Vector3 moveDirecion = new Vector3(direction.x, 0, direction.y);
